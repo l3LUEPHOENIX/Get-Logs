2857ded7b39e850c1f4127ab9b0b04653d4e3e85
l3LUEPHOENIX <61394752+l3LUEPHOENIX@users.noreply.github.com>
2021-10-17 13:18:37 -0500

    Create Get-Logs.ps1

---
+++ Get-Logs.ps1
@@ -0,0 +1,10 @@
+$searchPath = Read-Host -Prompt "Search Path"
+$keyWord = Read-Host -Prompt "Keyword"
+
+Get-ChildItem -Path $searchPath -Recurse | Foreach-Object {
+    if ((Select-String $_.FullName -Pattern $keyWord).Length -gt 0) {
+        echo "$("="*100)`r`n[FROM: $($_.FullName)]`r`n$("="*100)`r`n $(Get-Content $_ -Delimiter "`r`n" | Select-String $keyWord)`r`n"
+    }
+}
+
+echo "########## DONE ##########"

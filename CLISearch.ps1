95100e787b97e3744794b13d633315f87360bdd1
l3LUEPHOENIX <61394752+l3LUEPHOENIX@users.noreply.github.com>
2021-10-16 21:44:19 -0500

    Update CLISearch.ps1

--- CLISearch.ps1
+++ CLISearch.ps1
@@ -9,27 +9,36 @@ Function Get-Logs {
     Output the full path of the source document and all lines containing the keyword.
     
     .EXAMPLE
-    Get-Logs -Path 'C:\Users\User\Documents' -e .txt -w 'keyword'
+    Get-Logs -Path 'C:\Users\User\Documents' -Word 'keyword'
     
     .NOTES
     The narrower your search is, the faster you'll get your results.
     #>
     
     ### Parameters ###
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [Alias("Path")]
+        [string[]]$searchPath,
 
+        [Parameter(Mandatory)]
+        [Alias("Word")]
+        [string[]]$keyWord
+    )
     ###Header##
-    $defpath = ""
-    $defext = ".txt"
-    $extSet = ""
-    $keyWord = ""
-    $product = "";
+    #$defpath = ""
+    #$defext = ".txt"
+    #$extSet = ""
+    #$keyWord = ""
+    #$product = "";
 
     ### This is where most of the magic happens ###
     # This will recursively retreive all .log files underneath the target directory, which is assembled from the "$defpath" variable and the returns from the yearSearch and monthSearch functions.
     # Then it iterates through the found .log files, and if the keyword is found in the contents of any of the .log files, it will output the line the keyword was in and the file path of the .log file it came from.
-    Get-ChildItem -Path "$defpath\*$($extSet.Text)" -Recurse | Foreach-Object {
-        if ((Select-String $_.FullName -Pattern "$($keyWord.Text)").Length -gt 0) {
-            $product += "$("="*100)`r`n[FROM: $($_.FullName)]`r`n$("="*100)`r`n $(Get-Content $_ -Delimiter "`r`n" | Select-String "$($keyWord.Text)")`r`n"
+    Get-ChildItem -Path "$searchPath" -Recurse | Foreach-Object {
+        if ((Select-String $_.FullName -Pattern "$keyWord").Length -gt 0) {
+            Write-Host "$("="*100)`r`n[FROM: $($_.FullName)]`r`n$("="*100)`r`n $(Get-Content $_ -Delimiter "`r`n" | Select-String "$keyWord")`r`n"
         }
     }
 
